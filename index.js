b0921acf24ad3fb4c6e665736a6baf85a3894778
Sam Perozek <samperozek@gmail.com>
2022-06-08 12:11:16 -0500

    commit tree 7fd0d860abae4ae65b3c43322c8ecf52ae896814

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', woof);
+
+function woof(){
+    const text = document.getElementById('text');
+    text.textContent = "This is really cool!";
+}
+
+
